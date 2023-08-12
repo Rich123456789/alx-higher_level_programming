5dd65b94a5b3e2f1eb9a8e7e005a41f12d0da0e4
Rich123456789 <richard.obomanu818@gmail.com>
2023-08-12 15:18:21 -0700

    import and module

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+from add_0 import add
+
+"""Print the result of the addition of 1 and 2."""
+
+if __name__ == "__main__":
+
+	a = 1
+	b = 2
+
+	print("{} + {} = {}".format(a, b, add(a, b)))
