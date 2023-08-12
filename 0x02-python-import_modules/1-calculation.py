65a364a07583adaa4280e054ca5db8d80e2b70a2
Rich123456789 <richard.obomanu818@gmail.com>
2023-08-12 16:05:05 -0700

    import module

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+
+    """Print the sum from the addition of a and b."""
+
+    from calculator_1 import add
+    a = 10
+    b = 5
+    print("{} + {} = {}".format(a, b, add(a, b)))
+
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+
+    """Print the values from the subtraction of a and b."""
+
+    from calculator_1 import sub
+    a = 10
+    b = 5
+    print("{} + {} = {}".format(a, b, sub(a, b)))
+
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+
+    """Print the values from the multiplication of a and b."""
+
+    from calculator_1 import mul
+    a = 10
+    b = 5
+    print("{} + {} = {}".format(a, b, mul(a, b)))
+
+if __name__ == "__main__":
+
+    """Print the values from the division of a and b."""
+
+    from calculator_1 import div
+    a = 10
+    b = 5
+    print("{} + {} = {}".format(a, b, div(a, b)))
