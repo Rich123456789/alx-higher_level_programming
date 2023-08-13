5a945c44a2762c14f41c4cc69ca4fdb9472d031b
Rich123456789 <richard.obomanu818@gmail.com>
2023-08-13 06:13:01 -0700

    import and module

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -2,40 +2,14 @@
 
 if __name__ == "__main__":
 
-    """Print the sum from the addition of a and b."""
+	"""Print the add, sub, mul and div of a and b."""
 
-    from calculator_1 import add
-    a = 10
-    b = 5
-    print("{} + {} = {}".format(a, b, add(a, b)))
+from calculator_1 import add, sub, mul, div
 
-#!/usr/bin/python3
+a = 10
+b = 5
 
-if __name__ == "__main__":
-
-    """Print the values from the subtraction of a and b."""
-
-    from calculator_1 import sub
-    a = 10
-    b = 5
-    print("{} + {} = {}".format(a, b, sub(a, b)))
-
-#!/usr/bin/python3
-
-if __name__ == "__main__":
-
-    """Print the values from the multiplication of a and b."""
-
-    from calculator_1 import mul
-    a = 10
-    b = 5
-    print("{} + {} = {}".format(a, b, mul(a, b)))
-
-if __name__ == "__main__":
-
-    """Print the values from the division of a and b."""
-
-    from calculator_1 import div
-    a = 10
-    b = 5
-    print("{} + {} = {}".format(a, b, div(a, b)))
+print("{} + {} = {}".format(a, b, add(a, b)))
+print("{} + {} = {}".format(a, b, sub(a, b)))
+print("{} + {} = {}".format(a, b, mul(a, b)))
+print("{} + {} = {}".format(a, b, div(a, b)))
