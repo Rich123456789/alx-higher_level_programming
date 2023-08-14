a255e3116a9f585f2164fbfdbfc778e98da0bc0b
Rich123456789 <richard.obomanu818@gmail.com>
2023-08-14 15:35:59 -0700

    import and module

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__"
+   """Prints the result of the addition of all arguments."""
+
+   from sys import argv
+
+   addition = 0
+   for i in range(len(argv) - 1):
+     addition += int(argv[i = 1])
+   print("{}".format(addition))
