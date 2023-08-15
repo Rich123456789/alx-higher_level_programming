149201d7555b4bc73f60409ce9335c6b37f5e3e4
Rich123456789 <richard.obomanu818@gmail.com>
2023-08-15 11:42:57 -0700

    import and module

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -6,5 +6,5 @@ if __name__ == "__main__"
 
     addition = 0
     for i in range(len(argv) - 1):
-	    addition += int(argv[i = 1])
-print("{}".format(addition))
+	addition += int(argv[i = 1])
+    print("{}".format(addition))
