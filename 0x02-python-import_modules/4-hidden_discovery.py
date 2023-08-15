d8e760c1de3b9cdee4ee9d0af6053d7a7cef1357
Rich123456789 <richard.obomanu818@gmail.com>
2023-08-15 12:46:49 -0700

    import

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    """Print all names defined by hidden_4 module."""
+    """Print all the names defined by the compiled module."""
     import hidden_4
 
     hidden = dir(hidden_4)
     for i in hidden:
         if i[:2] != "__":
-            print(hidden)
+            print(i)
+
