8e112e98ed88b7fc23c88621633cde359edc0e37
Rich123456789 <richard.obomanu818@gmail.com>
2023-08-15 15:33:21 -0700

    import and module

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,22 +1,21 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    """Print all functions from a file and handles basic operations."""
-    from calculator_1 import add, sub, mul, div
+    """Print all functions from a file and handles basic operations"""
+    from calculator_1 import add, sub, mul, div 
     import sys
 
     num = len(sys.argv) - 1
 
-    if num != 3:
+    if len(sys.argv) - 1 != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
-    operators = {"+", "-", "*", "/"}
-
-    if sys.argv[2] not in list(operators.keys()):
+    ops = {"+": add, "-": sub, "*": mul, "/": div}
+    if sys.argv[2] not in list(ops.keys()):
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
 
     a = int(sys.argv[1])
     b = int(sys.argv[3])
-    print("{} {} {} = {}".format(a, sys.argv[2], b, operators[sys.argv[2]](a,b)))
+    print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
