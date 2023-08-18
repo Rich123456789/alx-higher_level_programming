481c6186e2439028753e5c50cd8517c55661797d
Rich123456789 <richard.obomanu818@gmail.com>
2023-08-18 03:30:09 -0700

    More data structure

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def square_matrix_simple(matrix=[]):
+	new_matrix = matrix.copy()
+
+	for i in range(len(matrix)):
+		new_matrix[i] = matrix.append(i**2)
+		print(new_matrix)
+		print(matrix)
